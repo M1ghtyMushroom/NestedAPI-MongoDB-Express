639d88aef496232735f31b157f38bf1d3fca90f2
M1ghtyMushroom <hilariousbananas@proton.me>
2024-01-15 23:26:28 +0200

    handled undefined routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const groupRouter = require('./routes/group');
+const resObj = require('./responses/resObj');
 
 const app = express();
 app.use(express.json());
 
 app.use('/groups', groupRouter);
+app.all('*', (req, res) => {
+  let msg = `#! Can't find ${req.originalUrl} on this server!`;
+  resObj.fail(res, 404, msg);
+});
 
 module.exports = app;
