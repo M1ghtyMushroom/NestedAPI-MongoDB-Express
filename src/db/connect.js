7bacbec70ad24390856340cb81ddc520c1a1f510
M1ghtyMushroom <hilariousbananas@proton.me>
2024-01-16 19:00:21 +0200

    removed unnecessary lines

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.DB, {
-    });
+    await mongoose.connect(process.env.DB, {});
     console.log('# Connected to database!');
   } catch (err) {
     console.error('#! Error connecting to database:', err.message);
